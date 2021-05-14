58c9f1fdc0645fe328d70bd2dafb68c9e6b277c5
farooa <Techm@2629>
2021-05-14 16:41:24 -0400

    Added Test for SIN validation test Successor

--- src/main/java/com/canadalife/automation/grsoe/pageobjects/BeneficiariesPO.java
+++ src/main/java/com/canadalife/automation/grsoe/pageobjects/BeneficiariesPO.java
@@ -10,7 +10,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.internal.DynamicGraphHelper;
 import ui.auto.core.components.WebComponent;
 import ui.auto.core.components.WebComponentList;
 import ui.auto.core.data.DataTypes;
